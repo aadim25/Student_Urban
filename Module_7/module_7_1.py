2867137e1c01a73b9f4deda7d0f752a19de143b8
aadim25 <aadim@mail.ru>
2024-11-20 09:41:00 +0700

    Ошибка в init Figure self.__sides

--- Module_7/module_7_1.py
+++ Module_7/module_7_1.py
@@ -1,28 +1,83 @@
+'''
+Задача "Учёт товаров":
+Необходимо реализовать 2 класса Product и Shop, с помощью которых будет производиться запись в файл с продуктами.
+Объекты класса Product будут создаваться следующим образом - Product('Potato', 50.0, 'Vagetables') и обладать следующими свойствами:
+Атрибут name - название продукта (строка).
+Атрибут weight - общий вес товара (дробное число) (5.4, 52.8 и т.п.).
+Атрибут category - категория товара (строка).
+Метод __str__, который возвращает строку в формате '<название>, <вес>, <категория>'. Все данные в строке разделены запятой с пробелами.
+
+Объекты класса Shop будут создаваться следующим образом - Shop() и обладать следующими свойствами:
+Инкапсулированный атрибут __file_name = 'products.txt'.
+Метод get_products(self), который считывает всю информацию из файла __file_name, закрывает его и возвращает единую строку со всеми товарами из файла __file_name.
+Метод add(self, *products), который принимает неограниченное количество объектов класса Product. Добавляет в файл __file_name каждый продукт из products, если его ещё нет в файле (по названию). Если такой продукт уже есть, то не добавляет и выводит строку 'Продукт <название> уже есть в магазине' .
+
+Пример результата выполнения программы:
+Пример работы программы:
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
+
+Вывод на консоль:
+Первый запуск:
+Spaghetti, 3.4, Groceries
+Potato, 50.5, Vegetables
+Spaghetti, 3.4, Groceries
+Potato, 5.5, Vegetables
+Второй запуск:
+Spaghetti, 3.4, Groceries
+Продукт Potato, 50.5, Vegetables уже есть в магазине
+Продукт Spaghetti, 3.4, Groceries уже есть в магазине
+Продукт Potato, 5.5, Vegetables уже есть в магазине
+Potato, 50.5, Vegetables
+Spaghetti, 3.4, Groceries
+Potato, 5.5, Vegetables
+
+'''
+
 from pprint import pprint
+
+
 class Product:
-    def __init__(self, name, weigth, category):
-        self.name=name
-        self.weigth=weigth
-        self.category=category
+    def __init__(self, name: str, weigth: float, category: str):
+        self.name = name
+        self.weigth = weigth
+        self.category = category
+
     def __str__(self):
         return (f'{self.name}, {self.weigth}, {self.category}')
+
+
 class Shop:
     def __init__(self):
         self.__file_name = 'products.txt'
 
     def get_products(self, words):
         file = open(self.__file_name)
-        pprint (file.read())
-         # = open_file_name.read('self.__file_name')
+        pprint(file.read())
         file.close()
         # return words
 
+
     def add(self, *products):
-        file = open(self.__file_name)
-        words = pprint (file.read())
+        file = open(self.__file_name, 'r')
+        words = file.read()
+        file.close()
         for i in products:
             if i in words:
-                print(f'Продукт {Product.name} уже есть в магазине' )
+                print(f'Продукт {Product.name} уже есть в магазине')
+            else:
+                file=open(self.__file_name,'w')
+                file.write(f'{self.name}, {self.weigth}, {self.category}')
+                file.close()
+
 # my_product = Product('Гречка','800 грамм','Крупа')
 # print(my_product.__str__())
 s1 = Shop()
@@ -30,7 +85,7 @@ p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2) # __str__
+print(p2)  # __str__
 
 s1.add(p1, p2, p3)
 
