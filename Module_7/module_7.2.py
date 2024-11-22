f3201475ac648a8664929a885097a939dfb08cd8
aadim25 <aadim@mail.ru>
2024-11-22 07:20:57 +0700

    Ошибка в init Figure self.__sides

--- Module_7/module_7.2.py
+++ Module_7/module_7.2.py
@@ -1,3 +1,8 @@
+'''
+https://urban-university.ru/members/courses/course999421818026/20231116-0000domasnee-zadanie-po-teme-pozicionirovanie-v-fajle-941056540023
+2023/11/16 00:00|Домашнее задание по теме "Позиционирование в файле".
+Цель: Закрепить знания о позиционировании в файле, использовав метод tell() файлового объекта. Написать усовершенствованную функцию записи.
+'''
 strings = ['111','223','333']
 file_name = 'temp7-2.txt'
 file_seek=[int]
@@ -6,13 +11,12 @@ def custom_write(file_name, strings:list):
     for i in strings:
         file_seek.append(file.tell())
         file.writelines(f'{i}\n')
-
     file.close()
     
 def my_dict(file_name):
     file = open(file_name, 'r', encoding='utf-8')
-    x=0
-    n=0
+    x=1
+    n=1
     text=''
     result=[{(int,int):str}]
     print (file_seek[0])
@@ -23,9 +27,9 @@ def my_dict(file_name):
         if (file.readline()) !="":
             text = (file.readline())
             print(text)
-            z=file_seek[x]
-            file.seek(z)
-            result.append({(x + 1, n): text})
+            # z=file_seek[x]
+            # file.seek(file_seek[0])
+            result.append({(x, n): text})
             print('n ',n,'txt ',text, 'x ',x)
             x += 1
         else:
