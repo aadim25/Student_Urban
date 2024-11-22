01ff33c6a8fc6308fd8a60e4321992c311374530
aadim25 <aadim@mail.ru>
2024-11-22 07:07:03 +0700

    Ошибка в init Figure self.__sides

--- Module_7/module_7.2.py
+++ Module_7/module_7.2.py
@@ -4,28 +4,28 @@ file_seek=[int]
 def custom_write(file_name, strings:list):
     file = open (file_name, 'w')
     for i in strings:
-        file.writelines(f'{i}\n')
         file_seek.append(file.tell())
+        file.writelines(f'{i}\n')
+
     file.close()
     
 def my_dict(file_name):
     file = open(file_name, 'r', encoding='utf-8')
-    # print(file.read())
-
-    # print(file.seek(5))
     x=0
     n=0
     text=''
     result=[{(int,int):str}]
+    print (file_seek[0])
     # print(result)
     my_bool = True
-    # file.seek(0)
+    file.seek(0)
     while my_bool:
         if (file.readline()) !="":
             text = (file.readline())
-            n=file.tell()
+            print(text)
+            z=file_seek[x]
+            file.seek(z)
             result.append({(x + 1, n): text})
-
             print('n ',n,'txt ',text, 'x ',x)
             x += 1
         else:
