07479e2dba95d515630fa9e056ae4e9054386090
aadim25 <aadim@mail.ru>
2024-11-29 15:12:27 +0700

    Доработка первого модуля

---
+++ module_1.py
@@ -0,0 +1,13 @@
+# 1st program
+print ((9**0.5)*5)
+# 2nd program
+print (9.98<9.99 and 1000 != 1001)
+# 3nd program
+print (2*2+2)
+print (2*(2+2))
+print ((2*2+2) == (2*(2+2)))
+# 4nd program
+my_str = '123.456'
+my_str = float(my_str)
+my_str = int((my_str - int(my_str))*10//1)
+print (my_str)
