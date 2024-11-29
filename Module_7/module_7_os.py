4a86ca7297b204fd7bbe3b585b9bceafe3c67a89
aadim25 <aadim@mail.ru>
2024-11-30 06:53:18 +0700

    Доработка первого модуля

---
+++ Module_7/module_7_os.py
@@ -0,0 +1,37 @@
+'''
+Задание:
+
+Создайте новый проект или продолжите работу в текущем проекте.
+Используйте os.walk для обхода каталога, путь к которому указывает переменная directory
+Примените os.path.join для формирования полного пути к файлам.
+Используйте os.path.getmtime и модуль time для получения и отображения времени последнего изменения файла.
+Используйте os.path.getsize для получения размера файла.
+Используйте os.path.dirname для получения родительской директории файла.
+
+Комментарии к заданию:
+
+Ключевая идея – использование вложенного for
+
+for root, dirs, files in os.walk(directory):
+  for file in files:
+    filepath = ?
+    filetime = ?
+    formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+    filesize = ?
+    parent_dir = ?
+    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+'''
+import os
+my_dir = []
+my_file = []
+# my_dir = os.getcwd()
+my_lst = os.walk(r'H:\Изображения')
+for root, dirs,files in my_lst:
+
+    for dir in dirs:
+        my_dir.append(dir)
+
+    for file in files:
+        my_file.append(file)
+
+for print (my_file)
