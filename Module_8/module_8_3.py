cfb2fd7b3a7cf9df3c07636e9f650acbf5a0252b
aadim25 <aadim@mail.ru>
2024-12-26 05:49:42 +0700

    нет ас

--- Module_8/module_8_3.py
+++ Module_8/module_8_3.py
@@ -104,6 +104,15 @@ else:
   print(f'{first.model} успешно создан')
 
 try:
+  second = Car('Model2', 300, 'т001тр')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{second.model} успешно создан')
+
+try:
   third = Car('Model3', 2020202, 'нет номера')
 except IncorrectVinNumber as exc:
   print(exc.message)
@@ -111,3 +120,4 @@ except IncorrectCarNumbers as exc:
   print(exc.message)
 else:
   print(f'{third.model} успешно создан')
+
