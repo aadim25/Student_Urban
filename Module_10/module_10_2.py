62bb0bf439b7f7a7851177bea6f80380db44914f
aadim25 <aadim@mail.ru>
2025-01-05 20:34:45 +0700

    нет ас

--- Module_10/module_10_2.py
+++ Module_10/module_10_2.py
@@ -50,24 +50,30 @@ Sir Lancelot одержал победу спустя 10 дней(дня)!
 Все битвы закончились!'''
 
 import time,threading
+
 class Knight(threading.Thread):
     def __init__(self,name,power):
         threading.Thread.__init__(self)
         self.name = str(name)
         self.power = int(power)
+        self.day = 0
+        self.enemies = 100
 
-    def __new__(self, name, power):
-        print(f'{self.name}, на нас напали!')
+    def run(self):
+        global enemies
+        print(f'{self.name}, на нас напали!.')
+        while self.enemies > 0:
+            time.sleep(1)
+            self.day += 1
+            print(f'{self.name} сражается {self.day} дней, воинов осталось {self.enemies}')
+            self.enemies-=self.power
+            if self.enemies ==0:
+                print (f'{self.name} одержал победу спустя {self.day} дней(дня)!')
+                return
 
-        
-
-for i in range(101):
-    first_knight = Knight('Sir Lancelot', 10)
-    second_knight = Knight("Sir Galahad", 20)
-
-    # first_knight.start()
-    # first_knight.join()
-    # second_knight.start()
-    # second_knight.join()
-
-    # print (i)
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+first_knight.start()
+second_knight.start()
+first_knight.join()
+second_knight.join()
